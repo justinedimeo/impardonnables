48b9694227b3c1d90d337e44234403cc35b17556
justinedimeo <dimeo.justine@gmail.com>
2017-03-30 21:41:45 +0200

    twitter

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,17 +1,15 @@
+/* HAMBURGER MENU */
+
 var burgerMenu = document.querySelector("#burgerMenu");
 var hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener(
   'click',
-  function()
-  {
-    if(hamburger.classList.contains("is-active"))
-      {
-        burgerMenu.style.left="-2000px";
-      }
-    else
-    {
-    burgerMenu.style.left="0px";
+  function () {
+    if (hamburger.classList.contains("is-active")) {
+      burgerMenu.style.left = "-2000px";
+    } else {
+      burgerMenu.style.left = "0px";
     }
     hamburger.classList.toggle("is-active");
   },
