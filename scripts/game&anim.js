92683542d9558a906a1395b1d1c3127ea942c479
justinedimeo <dimeo.justine@gmail.com>
2017-03-31 13:38:24 +0200

    last version

--- scripts/game&anim.js
+++ scripts/game&anim.js
@@ -9,18 +9,24 @@ function showQuestion() {
       document.getElementById('reponse').value = "";
     }
   } else {
+    if (secon > 59) { // Debug for the final timer
+      restTime = secon - 60;
+      secon = 0;
+      secon += restTime;
+      minu++
+    }
     game.style.display = "none"; //The game disappears
     fin.style.display = "block"; //end of the game 
-    fin.innerHTML = ("You Finished in " + minu + "m" + secon + "s"); //Displays the score (timer)
+    fin.innerHTML = ("You Finished in " + minu + ":" + secon + ":" + centi); //Displays the score (timer)
     document.querySelector("#mainText").style.display = "block";
     document.querySelector("#submit").style.display = "block";
-    document.querySelector("#linkclassement").style.display = "block";
+    document.querySelector("#yourName").style.display = "block"; document.querySelector("#linkclassement").style.display = "block";
 
 
   }
 }
 
-////////////////////////////////Indication////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////INDICATION////////////////////////////////////////////////////////////////////////////////////////////
 indice.addEventListener(
   'click',
   function () {
@@ -39,15 +45,13 @@ document.getElementById('reponse').addEventListener('keypress', function (event)
     indiceView.style.display = "none";
     if (reponse == motsDef[def.indexOf(question)]) { //If answer is equal to the response of the same index as the question
       score++;
-      document.getElementById('question').style.left = "40%";
+      document.getElementById('question').style.left = "40%"; // Move of the block question : answer is true
       setTimeout(function () {
-        document.getElementById('question').style.left = "30%";
-
+        document.getElementById('question').style.left = "30%"; //back to the initial position
       }, 300);
-
-      document.querySelector('#question').style.backgroundColor = "#B1D696"
+      document.querySelector('#question').style.backgroundColor = "#B1D696"; //change color in green : answer is true
       setTimeout(function () {
-        document.querySelector('#question').style.backgroundColor = "white";
+        document.querySelector('#question').style.backgroundColor = "white"; // back to the initial color
 
       }, 300);
 
@@ -55,28 +59,23 @@ document.getElementById('reponse').addEventListener('keypress', function (event)
       prof.style.transform = "rotate(-60deg)"; /////// Character Rotation
       setTimeout(function () {
         prof.style.transform = "rotate(0deg)";
-
       }, 100);
-      document.getElementById('question').style.left = "15%";
+      document.getElementById('question').style.left = "15%"; // Move of the block question : answer is false
       setTimeout(function () {
-        document.getElementById('question').style.left = "30%";
+        document.getElementById('question').style.left = "30%"; //back to the initial position
+      }, 300);
+      document.querySelector('#question').style.backgroundColor = "#FF8E8E"; //change color in red : answer is false
+      setTimeout(function () {
+        document.querySelector('#question').style.backgroundColor = "white"; // back to the initial color
 
       }, 300);
-
-      document.querySelector('#question').style.backgroundColor = "#FF8E8E";
-      setTimeout(function () {
-        document.querySelector('#question').style.backgroundColor = "white";
-
-      }, 300);
-
-      correction.textContent += "   / " + motsDef[aleasecondaire];
-
-      secon += 25;
+      correction.textContent += "   / " + motsDef[aleasecondaire]; // For the training mode
+      secon += 25; // add seconds when your answer is false
 
     }
 
     counter++;
-    document.querySelector(".chronoVisible").innerHTML = counter + "/10";
+    document.querySelector(".chronoVisible").innerHTML = counter + "/10"; //show you your progression in the game : you can change the "10" if you want to change the number of definition you want 
     showQuestion();
   }
 });
